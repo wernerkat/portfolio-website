dba984ba722f42dbe962e622c5a174ba81b3f24b
wernerkat <wernerkat@gmail.com>
2022-01-12 13:50:45 -0700

    Add styling for hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+if (window.chrome)
+// hamburgermenu
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}

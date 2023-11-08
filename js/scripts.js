c804aeda7e172f19f6251047033b4397b9e22ff7
wernerkat <wernerkat@gmail.com>
2023-11-07 17:37:14 -0700

    add more better

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,5 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
-
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
-
-document.getElementById('video').play();
+// arrow
+smoothScroll.init();
